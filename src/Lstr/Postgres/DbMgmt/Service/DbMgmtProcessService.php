02fb34640e17390a2b3b63aea0334e1483a9b652
Matt Light <matt.light@lightdatasys.com>
2016-02-28 21:32:29 -0800

    Add DB restore process

--- src/Lstr/Postgres/DbMgmt/Service/DbMgmtProcessService.php
+++ src/Lstr/Postgres/DbMgmt/Service/DbMgmtProcessService.php
@@ -54,4 +54,74 @@ class DbMgmtProcessService
 
         return $return;
     }
+
+    /**
+     * @param string $host_key
+     * @param string $database
+     * @param string $source_path
+     * @return array
+     */
+    public function restore($host_key, $database, $source_path)
+    {
+        $host = $this->db_host_manager->getHost($host_key);
+
+        $create_response = $this->create($host, $database);
+        if ($create_response['exitCode']) {
+            return $create_response;
+        }
+
+        $command = $host->getPathToPgBin('pg_restore')
+            . " -U " . escapeshellarg($host->getUsername())
+            . " -h " . escapeshellarg($host->getHostname())
+            . " -F c -d " . escapeshellarg($database)
+            . " -j " . escapeshellarg($host->getOption('import_processes', 1))
+            . " -v " . escapeshellarg($source_path);
+
+        $process = new Process($command);
+        $process->setTimeout(60 * 60 * 6); // 6 hours
+        $process->setIdleTimeout(60 * 30); // 30 minutes
+
+        $process->run();
+
+        $return = [
+            'exitCode' => $process->getExitCode(),
+        ];
+
+        if ($return['exitCode']) {
+            $return['stderr'] = $process->getErrorOutput();
+            $return['command'] = $command;
+        }
+
+        return $return;
+    }
+
+    /**
+     * @param $host
+     * @param $database
+     * @return array
+     */
+    private function create($host, $database)
+    {
+        $command = $host->getPathToPgBin('createdb')
+            . " -U " . escapeshellarg($host->getUsername())
+            . " -h " . escapeshellarg($host->getHostname())
+            . " " . escapeshellarg($database);
+
+        $process = new Process($command);
+        $process->setTimeout(60); // 1 minute
+        $process->setIdleTimeout(60); // 1 minute
+
+        $process->run();
+
+        $return = [
+            'exitCode' => $process->getExitCode(),
+        ];
+
+        if ($return['exitCode']) {
+            $return['stderr'] = $process->getErrorOutput();
+            $return['command'] = $command;
+        }
+
+        return $return;
+    }
 }
